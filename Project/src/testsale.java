c2c35061ca153a5f0e18af21ee01fe12afa5d866
AhmedSabanovic <92882375+AhmedSabanovic@users.noreply.github.com>
2022-12-24 01:17:55 +0100

    Update testsale.java

--- Project/src/testsale.java
+++ Project/src/testsale.java
@@ -5,23 +5,21 @@ import pages.Homepage;
 
 public class testsale {
     public static void main(String[] args) throws InterruptedException {
-        // Create a new WebDriver object
+ 
         WebDriver driver = new EdgeDriver();
         driver.get("https://espresso.ba/");
         driver.manage().window().maximize();
 
-        // Create a new Homepage object
         Homepage homepage = new Homepage(driver);
 
         homepage.saleButton();
-        // Check the result of the register attempt
+     
         if (driver.getCurrentUrl().contains("https://espresso.ba/snizeno/")) {
             System.out.println("Sale page is opened successfully");
         } else {
             System.out.println("Unexpected result while opening sale page");
         }
 
-        // Close the browser
         driver.quit();
     }
 }
